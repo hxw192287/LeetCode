0b19c0d6b24fed2b9649271a7f0349f757c9a6f5
PoisonX_X <hxw192287@gmail.com>
2018-03-27 14:27:06 -0400

    Update rank-scores.sql

--- MySQL/rank-scores.sql
+++ MySQL/rank-scores.sql
@@ -43,3 +43,9 @@ SELECT Ranks.Score, Ranks.Rank FROM Scores LEFT JOIN
  SELECT Score,  (SELECT COUNT(DISTINCT(Score)) FROM  Scores b WHERE b.Score > a.Score) + 1 AS Rank
        FROM Scores a
        ORDER by Score DESC
+#or
+SELECT
+  Score,
+  (SELECT count(*) FROM (SELECT distinct Score s FROM Scores) tmp WHERE s >= Score) Rank
+FROM Scores
+ORDER BY Score desc
