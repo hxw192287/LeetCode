0572d60c350212bbdd9c3b9bb96a6ed2acf38750
PoisonX_X <hxw192287@gmail.com>
2018-03-29 17:07:50 -0400

    Update department-highest-salary.sql

--- MySQL/department-highest-salary.sql
+++ MySQL/department-highest-salary.sql
@@ -40,3 +40,10 @@ FROM (SELECT Department.Id AS DepartmentId, Department.Name AS Department, emp.S
 SELECT Department.Name AS Department, Employee.Name AS Employee, Employee.Salary AS Salary
 FROM Department JOIN Employee ON Employee.DepartmentId = Department.Id
 WHERE Employee.Salary IN (SELECT MAX(e.Salary) FROM Employee e WHERE e.DepartmentId = Employee.DepartmentId)
+
+SELECT D.Name as Department, E.Name as Employee, E.Salary 
+  FROM Department D, Employee E, Employee E2  
+  WHERE D.ID = E.DepartmentId and E.DepartmentId = E2.DepartmentId and 
+  E.Salary <= E2.Salary
+  group by D.ID,E.Name having count(distinct E2.Salary) = 1
+  order by D.Name desc
