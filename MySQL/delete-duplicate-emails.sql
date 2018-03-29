5541400d9e02b87a9cc6a7da4bb0453bdc0f2b31
PoisonX_X <hxw192287@gmail.com>
2018-03-29 14:52:40 -0400

    Update delete-duplicate-emails.sql

--- MySQL/delete-duplicate-emails.sql
+++ MySQL/delete-duplicate-emails.sql
@@ -26,3 +26,10 @@
 DELETE p1
 FROM Person p1, Person p2
 WHERE p1.Email = p2.Email AND p1.Id > p2.Id
+
+#or
+delete from Person where id not in( 
+    select t.id from (
+        select min(id) as id from Person group by email
+    ) t
+)
