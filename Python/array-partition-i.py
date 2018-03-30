df5051f078002f62dc9e4e236c92c08e32b6ebab
PoisonX_X <hxw192287@gmail.com>
2018-03-30 12:35:26 -0400

    Update array-partition-i.py

--- Python/array-partition-i.py
+++ Python/array-partition-i.py
@@ -12,7 +12,15 @@
 # Note:
 # n is a positive integer, which is in the range of [1, 10000].
 # All the integers in the array will be in the range of [-10000, 10000].
-
+# 
+# Proof:
+# Assume in each pair i, bi >= ai.
+# Denote Sm = min(a1, b1) + min(a2, b2) + ... + min(an, bn). The biggest Sm is the answer of this problem. Given 1, Sm = a1 + a2 + ... + an.
+# Denote Sa = a1 + b1 + a2 + b2 + ... + an + bn. Sa is constant for a given input.
+# Denote di = |ai - bi|. Given 1, di = bi - ai. Denote Sd = d1 + d2 + ... + dn.
+# So Sa = a1 + a1 + d1 + a2 + a2 + d2 + ... + an + an + dn = 2Sm + Sd => Sm = (Sa - Sd) / 2. To get the max Sm, given Sa is constant, we need to make Sd as small as possible.
+# So this problem becomes finding pairs in an array that makes sum of di (distance between ai and bi) as small as possible. Apparently, sum of these distances of adjacent elements is the smallest. If that’s not intuitive enough, see attached picture. Case 1 has the smallest Sd.
+# 0_1492961937328_leetcode561.jpg
 class Solution(object):
     def arrayPairSum(self, nums):
         """
