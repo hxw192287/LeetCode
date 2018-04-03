4f7136e0408158bcbd4bbd7e4cbc05c6878ad15a
PoisonX_X <hxw192287@gmail.com>
2018-04-03 14:30:41 -0400

    Update wiggle-sort.py

--- Python/wiggle-sort.py
+++ Python/wiggle-sort.py
@@ -13,7 +13,7 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         for i in xrange(1, len(nums)):
-            if ((i % 2) and nums[i - 1] > nums[i]) or \
-                (not (i % 2) and nums[i - 1] < nums[i]):
+            if ((i&1!=0) and nums[i - 1] > nums[i]) or \
+                ((i&1==0) and nums[i - 1] < nums[i]):
                 # Swap unordered elements.
                 nums[i - 1], nums[i] = nums[i], nums[i - 1]
