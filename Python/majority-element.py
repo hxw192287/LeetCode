7c5f68a0fec3d566f3eed82966054273ccd3e908
PoisonX_X <hxw192287@gmail.com>
2018-04-19 12:55:39 -0400

    Update majority-element.py

--- Python/majority-element.py
+++ Python/majority-element.py
@@ -33,6 +33,23 @@ class Solution:
         :rtype: int
         """
         return sorted(collections.Counter(nums).items(), key=lambda a: a[1], reverse=True)[0][0]
+    
+    def majorityElement3(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        if n==1 :
+            return nums[0]
+        if n%2 :
+            find = set(nums[0:(n//2)+1]) & set(nums[n//2:]) ／／intersection 
+        else:
+            find = set(nums[0:n//2]) & set(nums[n//2:])
+        
+        for i in find:
+            if nums.count(i)>n//2:
+                return i
 
 if __name__ == "__main__":
     print Solution().majorityElement([1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 6])
