822e3fae519d2b4e3573f0ed6c9bd3e6aeb085e7
PoisonX_X <hxw192287@gmail.com>
2018-05-08 12:43:05 -0400

    Update maximum-distance-in-arrays.py

--- Python/maximum-distance-in-arrays.py
+++ Python/maximum-distance-in-arrays.py
@@ -1,3 +1,18 @@
+# Description
+
+# Given m arrays, and each array is sorted in ascending order. Now you can pick up two integers from two different arrays
+# (each array picks one) and calculate the distance. We define the difference between two integers a and b 
+# to be their absolute difference |a-b|. Your task is to find the maximum difference.
+
+# Note: Each given array will have at least 1 number. There will be at least two non-empty arrays.
+# The total number of the integers in all the m arrays will be in the range of [2, 10000].
+# The integers in the m arrays will be in the range of [-10000, 10000].
+
+# Example
+# Given arrays = [[1,2,3], [4,5], [1,2,3]]
+# Return 4
+# One way to reach the maximum difference 4 is to pick 1 in the first or third array and pick 5 in the second array.
+
 # Time:  O(n)
 # Space: O(1)
 
@@ -15,3 +30,4 @@ class Solution(object):
             min_val = min(min_val, arrays[i][0])
             max_val = max(max_val, arrays[i][-1])
         return result
+ 
