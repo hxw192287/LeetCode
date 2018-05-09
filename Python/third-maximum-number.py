ea3c9efe27296119a253226c5f05dc38c63f14cc
PoisonX_X <hxw192287@gmail.com>
2018-05-09 12:03:44 -0400

    Update third-maximum-number.py

--- Python/third-maximum-number.py
+++ Python/third-maximum-number.py
@@ -28,3 +28,17 @@ class Solution(object):
             return top[0]
 
         return top[2]
+
+  class Solution2:
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        v = [float('-inf'), float('-inf'), float('-inf')]
+        for num in nums:
+            if num not in v:
+                if num > v[0]:   v = [num, v[0], v[1]]
+                elif num > v[1]: v = [v[0], num, v[1]]
+                elif num > v[2]: v = [v[0], v[1], num]
+        return max(nums) if float('-inf') in v else v[2]
