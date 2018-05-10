7802a1e5318bf53885b00a83f47158907ec5e29c
PoisonX_X <hxw192287@gmail.com>
2018-05-10 17:14:36 -0400

    Update combination-sum-iii.py

--- Python/combination-sum-iii.py
+++ Python/combination-sum-iii.py
@@ -43,3 +43,24 @@ class Solution:
             self.combinationSumRecu(result, intermediate, start + 1, k - 1, target - start)
             intermediate.pop()
             start += 1
+
+  
+# OR
+class Solution2:
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(range(1,10), k, n, 0, [], res)
+        return res
+    
+    def dfs(self, nums, k, n, index, path, res):
+        if k < 0 or n < 0:
+            return
+        if k == 0 and n == 0:
+            res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, k-1, n-nums[i], i+1, path+[nums[i]], res)
