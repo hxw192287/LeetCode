e05d48295d4b49dd0d980e0a5258d89db8630630
PoisonX_X <hxw192287@gmail.com>
2018-05-23 14:49:56 -0400

    Update median-of-two-sorted-arrays.py

--- Python/median-of-two-sorted-arrays.py
+++ Python/median-of-two-sorted-arrays.py
@@ -80,6 +80,36 @@ class Solution_Generic(object):
                 right = max(right, array[-1])
 
         return binary_search(arrays, left, right, k, match)
+       
+ class Solution3(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if len(nums2) < len(nums1): return self.findMedianSortedArrays(nums2, nums1)
+        cut1, cut2, cutL, cutR = 0, 0, 0, len(nums1)
+        length = len(nums1) + len(nums2)
+        while cut1 <= len(nums1):
+            cut1 = (cutR - cutL)/2 + cutL
+            cut2 = length /2 - cut1
+            L1 = float('-inf') if cut1 == 0 else nums1[cut1 - 1]
+            L2 = float('-inf') if cut2 == 0 else nums2[cut2 - 1]
+            R1 = float('inf') if cut1 == len(nums1) else nums1[cut1]
+            R2 = float('inf') if cut2 == len(nums2) else nums2[cut2]
+            if L1 > R2: 
+                cutR = cut1 - 1
+            elif L2 > R1:
+                cutL = cut1 + 1
+            else:
+                if length%2 == 0:
+                    L1 = L1 if L1 > L2 else L2
+                    R1 = R1 if R1 < R2 else R2
+                    return (L1 + R1)/2.0
+                else:
+                    R1 = R1 if R1 < R2 else R2
+                    return R1
 
 
 if __name__ == "__main__":
