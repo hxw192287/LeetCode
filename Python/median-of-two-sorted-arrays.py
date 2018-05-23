efa1672b81c8a8068ccf3c3f2af597eb94a5513a
PoisonX_X <hxw192287@gmail.com>
2018-05-23 14:51:34 -0400

    Update median-of-two-sorted-arrays.py

--- Python/median-of-two-sorted-arrays.py
+++ Python/median-of-two-sorted-arrays.py
@@ -37,6 +37,40 @@ class Solution(object):
 
         return max(Ai_minus_1, Bj)
 
+class Solution2(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        A, B, m, n = nums1, nums2, len(nums1), len(nums2)
+        if m > n: A, B, m, n = B, A, n, m
+        if n == 0: raise ValueError
+        
+        imin, imax, half_len = 0, m, (m+n+1)/2
+        
+        while imin <= imax:
+            i = (imin + imax) / 2
+            j = half_len - i
+            
+            if i < m and B[j-1] > A[i]:
+                imin = i+1
+            elif i > 0  and A[i-1] > B[j]:
+                imax = i-1
+            else:
+                if i == 0: max_of_left = B[j-1]
+                elif j == 0: max_of_left = A[i-1]
+                else: max_of_left = max(B[j-1], A[i-1])
+                
+                if (m+n)%2 == 1:
+                    return max_of_left
+                
+                if i == m: min_of_right = B[j]
+                elif j == n: min_of_right = A[i]
+                else: min_of_right = min(B[j], A[i])
+                
+                return (max_of_left+min_of_right)/2.0
 
 # Time:  O(log(max(m, n)) * log(max_val - min_val))
 # Space: O(1)
